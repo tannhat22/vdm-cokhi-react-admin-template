d8cd036ff0f0bfaac85aaed40de859c5e97af9df
tannhat <nguyentannhat2298@gmail.com>
2024-02-23 19:26:36 +0700

    new

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -21,6 +21,7 @@ import { openDrawer } from 'store/reducers/menu';
 const MainLayout = () => {
   // const theme = useTheme();
   // const matchDownLG = useMediaQuery(theme.breakpoints.down('lg'));
+
   const dispatch = useDispatch();
 
   const { drawerOpen } = useSelector((state) => state.menu);
